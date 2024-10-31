eb4edddce2550264a4344b8eed9b600f18a58883
Saltanat Kydykbekova <saltanat.kydykbekova1@gmail.com>
2024-10-31 18:32:04 +0600

    commit added seconds

--- assets 16/main.js
+++ assets 16/main.js
@@ -57,15 +57,16 @@ function realtimeClock() {
 
     var hours = currTime.getHours();
     var minutes = currTime.getMinutes();
-
+    var seconds = currTime.getSeconds();
     var amPm = ( hours < 12 ) ? "AM" : "PM";
 
     hours = (hours > 12) ? hours - 12 : hours;
 
     hours = ("0" + hours).slice(-2);
     minutes = ("0" + minutes).slice(-2);
+    seconds = ("0" + seconds).slice(-2);
 
-    clock.innerHTML = `${hours} : ${minutes} ${amPm}`;
+    clock.innerHTML = `${hours} : ${minutes} :${seconds} ${amPm}`;
     setTimeout(realtimeClock, 500);
 }
 realtimeClock();
